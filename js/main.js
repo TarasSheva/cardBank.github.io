91898d04f19c551fda843a2dff6d4d171a7df552
TarasSheva <tarasgsheva@gmail.com>
2020-11-15 18:33:47 +0200

    final

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar_menu')
+const navbarLogo = document.querySelector('#navbar_logo')
 
 const mobileMenu = () => {
     menu.classList.toggle('is-active')
@@ -7,3 +8,46 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu)
+
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home_page');
+    const aboutMenu = document.querySelector('#about_page');
+    const servicesMenu = document.querySelector('#services_page');
+    let scrollPos = window.scrollY;
+    // console.log(scrollPos);
+
+    if (window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        servicesMenu.classList.remove('highlight');
+        return;
+    } else if (window.innerWidth > 960 && scrollPos < 2345) {
+        servicesMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
+    }
+
+    if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+        elem.classList.remove('highlight');
+    }
+}
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+//  Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navbarLogo.addEventListener('click', hideMobileMenu);
